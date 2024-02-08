58b9b5556f64222edc06fa5fc6a52aa405cf9ab1
Alieu Jagne <ajagne01@gmail.com>
2024-02-08 18:27:02 +0100

    ignor local eng

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+import GenerateButton from "@/components/GenerateButton";
+import { Tldraw } from "@tldraw/tldraw";
+
 export default function Home() {
-  return "home";
+  return (
+    <main className=" w-screen">
+      <Tldraw persistenceKey="snapapp">
+        <div className="h-full w-full flex items-center justify-center">
+          <h1 className="text-4xl font-bold">Welcome to Snapapp</h1>
+          <GenerateButton />
+        </div>
+      </Tldraw>
+    </main>
+  );
 }
--- components/GenerateButton.tsx
+++ components/GenerateButton.tsx
@@ -1,3 +1,12 @@
+import { CogIcon } from "lucide-react";
+
 export default function GenerateButton() {
-  return "generate button";
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-lg z-[1000] absolute top-4 left-1/2 -translate-x-1/2 shadow-md shadow-blue-800/50">
+      <span className="inline-flex items-center">
+        <CogIcon className="w-4 h-4 mr-1" />
+        <span>Generate</span>
+      </span>
+    </button>
+  );
 }
